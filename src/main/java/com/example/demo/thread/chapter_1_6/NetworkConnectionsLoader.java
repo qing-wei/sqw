692d7bd240ecdee941fbce2216e3894ce30e3f8f
siqingwei <siqingwei@yeah.net>
2018-06-25 16:12:23 +0800

    等待线程的终结

---
+++ src/main/java/com/example/demo/thread/chapter_1_6/NetworkConnectionsLoader.java
@@ -0,0 +1,20 @@
+package com.example.demo.thread.chapter_1_6;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by siqingwei on 2018/6/25.
+ */
+public class NetworkConnectionsLoader implements Runnable {
+    @Override
+    public void run() {
+        System.out.printf("Beginning network loading : %s\n", new Date());
+        try {
+            TimeUnit.SECONDS.sleep(6);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Ending network loading : %s\n", new Date());
+    }
+}
