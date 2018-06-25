c05a1241a2df5ddc88d9d7f709bd93e83d5b4a8e
siqingwei <siqingwei@yeah.net>
2018-06-25 14:51:13 +0800

    处理异常时，使线程中断执行

--- src/main/java/com/example/demo/thread/chapter_1_5/FileClockThread.java
+++ src/main/java/com/example/demo/thread/chapter_1_5/FileClockThread.java
@@ -11,13 +11,14 @@ public class FileClockThread extends Thread {
     public void run() {
         super.run();
         for (int i = 0; i < 10; i++) {
-            System.out.println(isInterrupted());
 
             System.out.printf("%s\n", new Date());
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 System.out.println("The FileClock has been interrupted.");
+                System.out.println(isInterrupted());
+                return;
             }
         }
     }
