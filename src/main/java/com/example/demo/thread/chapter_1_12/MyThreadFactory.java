b33b7839788e58c297d64b8b5bd0a800eea45740
siqingwei <siqingwei@yeah.net>
2018-06-26 15:15:53 +0800

    用线程工厂创建线程

---
+++ src/main/java/com/example/demo/thread/chapter_1_12/MyThreadFactory.java
@@ -0,0 +1,40 @@
+package com.example.demo.thread.chapter_1_12;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Created by siqingwei on 2018/6/26.
+ */
+public class MyThreadFactory implements ThreadFactory {
+
+    private int counter;
+    private String name;
+    private List<String> strList;
+
+    public MyThreadFactory(String name) {
+        this.counter = 0;
+        this.name = name;
+        this.strList = new ArrayList<>();
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread thread = new Thread(r, name + "_Thread-" + counter);
+        counter++;
+        String str = String.format("created thread %s with name %s on %s.", thread.getId(), thread.getName(), new Date());
+        strList.add(str);
+        return thread;
+    }
+
+    public String getStrList(){
+        StringBuffer stringBuffer = new StringBuffer();
+        for (String str : strList) {
+            stringBuffer.append(str);
+            stringBuffer.append("\n");
+        }
+        return stringBuffer.toString();
+    }
+}
