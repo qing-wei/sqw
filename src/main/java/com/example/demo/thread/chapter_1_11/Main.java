4a66ce72eae79222daf7a7615c8f8e039cbc1840
siqingwei <siqingwei@yeah.net>
2018-06-26 14:35:20 +0800

    处理线程组内的不受控制异常

---
+++ src/main/java/com/example/demo/thread/chapter_1_11/Main.java
@@ -0,0 +1,15 @@
+package com.example.demo.thread.chapter_1_11;
+
+/**
+ * Created by siqingwei on 2018/6/26.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Task task = new Task();
+        MyThreadGroup myThreadGroup = new MyThreadGroup("MyThreadGroup");
+        for (int i = 0; i < 2; i++) {
+            Thread thread = new Thread(myThreadGroup, task);
+            thread.start();
+        }
+    }
+}
