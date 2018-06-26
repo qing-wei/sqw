118f4058f02d0ebcb2723270c6302865e5450eba
siqingwei <siqingwei@yeah.net>
2018-06-26 14:06:07 +0800

    线程组

---
+++ src/main/java/com/example/demo/thread/chapter_1_10/Result.java
@@ -0,0 +1,16 @@
+package com.example.demo.thread.chapter_1_10;
+
+/**
+ * Created by siqingwei on 2018/6/26.
+ */
+public class Result {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
