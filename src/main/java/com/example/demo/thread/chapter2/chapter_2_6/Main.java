32b3ac30d627088523495ce38f30c043f0ccfd74
siqingwei <siqingwei@yeah.net>
2018-06-27 15:23:24 +0800

    修改Lock的公平性

---
+++ src/main/java/com/example/demo/thread/chapter2/chapter_2_6/Main.java
@@ -0,0 +1,19 @@
+package com.example.demo.thread.chapter2.chapter_2_6;
+
+/**
+ * Created by siqingwei on 2018/6/27.
+ */
+public class Main {
+    public static void main(String[] args) {
+        PrintQueue printQueue = new PrintQueue();
+
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            threads[i] = new Thread(new Job(printQueue), "Thread" + i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            threads[i].start();
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/thread/chapter2/chapter_2_6/PrintQueue.java
@@ -0,0 +1,37 @@
+package com.example.demo.thread.chapter2.chapter_2_6;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by siqingwei on 2018/6/27.
+ */
+public class PrintQueue {
+    private final Lock queueLock = new ReentrantLock(true);
+
+    public void printJob(Object document) {
+        queueLock.lock();
+        try {
+            long duration = (long) (Math.random() * 10000);
+            System.out.println(Thread.currentThread().getName() + ": PrintQueue : print a job during : " + (duration/1000) + " seconds");
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("----------");
+            queueLock.unlock();
+        }
+
+        queueLock.lock();
+        try {
+            long duration = (long) (Math.random() * 10000);
+            System.out.println(Thread.currentThread().getName() + ": PrintQueue : print a job during : " + (duration/1000) + " seconds");
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("----------");
+            queueLock.unlock();
+        }
+    }
+}
