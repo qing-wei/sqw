25d8f579d5cd9cc7c0bd28c349a49944d1d3d006
siqingwei <siqingwei@yeah.net>
2018-08-16 15:04:15 +0800

    执行者取消一个任务

--- src/main/java/com/example/demo/thread/chapter4/chapter_4_8/Main.java
+++ src/main/java/com/example/demo/thread/chapter4/chapter_4_8/Main.java
@@ -1,7 +1,35 @@
 package com.example.demo.thread.chapter4.chapter_4_8;
 
+import java.util.concurrent.*;
+
 /**
  * Created by siqingwei on 2018/8/16.
  */
 public class Main {
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+        Task task = new Task();
+        System.out.println("Main : Executing the Task.");
+        Future<String> future = executor.submit(task);
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Main : Canceling the Task.");
+        future.cancel(true);
+        System.out.printf("Main : Canceled : %s.\n", future.isCancelled());
+        System.out.printf("Main : Done : %s.\n", future.isDone());
+//        try {
+//            future.get();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
+        executor.shutdown();
+        System.out.printf("Main : The Executor has finished.\n");
+    }
 }
--- src/main/java/com/example/demo/thread/chapter4/chapter_4_8/Task.java
+++ src/main/java/com/example/demo/thread/chapter4/chapter_4_8/Task.java
@@ -1,7 +1,16 @@
 package com.example.demo.thread.chapter4.chapter_4_8;
 
+import java.util.concurrent.Callable;
+
 /**
  * Created by siqingwei on 2018/8/16.
  */
-public class Task {
+public class Task implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        while (true) {
+            System.out.println("Task Test.");
+            Thread.sleep(100);
+        }
+    }
 }
