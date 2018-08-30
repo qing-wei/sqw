7c05d5bdacf4d8c7a2e5bb0fbc5028a573d18cbb
siqingwei <siqingwei@yeah.net>
2018-08-30 10:52:09 +0800

    单元测试

--- src/test/java/com/example/demo/test/IntegerTest.java
+++ src/test/java/com/example/demo/test/IntegerTest.java
@@ -4,4 +4,16 @@ package com.example.demo.test;
  * Created by siqingwei on 2018/8/20.
  */
 public class IntegerTest {
+    public static void main(String[] args) {
+        Integer a = new Integer(129);
+        Integer b = new Integer(129);
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+
+        System.out.println(a == 129);
+        Integer c = 17;
+        Integer d = new Integer(17);
+        System.out.println(c == d);
+        System.out.println(c.equals(d));
+    }
 }
--- src/test/java/com/example/demo/thread/Test.java
+++ src/test/java/com/example/demo/thread/Test.java
@@ -4,4 +4,18 @@ package com.example.demo.thread;
  * Created by siqingwei on 2018/7/3.
  */
 public class Test {
+    public static void main(String[] args) {
+//        long begin = System.currentTimeMillis();
+//        for (int i = 0; i < 100000; i++) {
+//            createOrderSn(i);
+//        }
+//        long end = System.currentTimeMillis();
+//        System.out.println((double)(begin-end)/100000);
+//        System.out.println(System.currentTimeMillis());
+        System.out.println(createOrderSn(88888888));
+    }
+
+    private static String createOrderSn(int i){
+        return String.format("%07d",i) + System.currentTimeMillis();
+    }
 }
