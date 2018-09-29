488e308bf9d4fded6f36e4d85a6984916958cfad
siqingwei <siqingwei@yeah.net>
2018-09-29 18:50:02 +0800

    并发执行与串行执行耗时测试

---
+++ src/main/java/com/example/demo/artconcurrent/ConcurrencyTest.java
@@ -0,0 +1,53 @@
+package com.example.demo.artconcurrent;
+
+/**
+ * Created by siqingwei on 2018/9/29.
+ */
+public class ConcurrencyTest {
+    /** 执行次数 */
+    private static final long  count = 100000000L;
+
+    public static void main(String[] args) throws InterruptedException {
+        // 并发计算
+        concurrency();
+        // 单线程计算
+        serial();
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        System.out.println(a);
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial: " + time + "ms, b = " + b);
+    }
+
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+                System.out.println(a);
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        thread.join();
+        long time = System.currentTimeMillis() - start;
+        System.out.println("concurrency: " + time + "ms, b = " + b);
+    }
+}
